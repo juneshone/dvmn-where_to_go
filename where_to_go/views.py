445b115e364f08b86da02f47258460ecc78d1646
juneshone <shamrova.julia@mail.ru>
2024-06-10 01:39:02 +0700

    Add places from DB

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -3,7 +3,6 @@ from places.models import Place
 
 
 def show_start_page(request):
-    Place.objects.all()
     places = {
       "type": "FeatureCollection",
       "features": [
@@ -11,26 +10,14 @@ def show_start_page(request):
           "type": "Feature",
           "geometry": {
             "type": "Point",
-            "coordinates": [37.62, 55.793676]
+            "coordinates": [place.lng, place.lat]
           },
           "properties": {
-            "title": "«Легенды Москвы",
-            "placeId": "moscow_legends",
-            "detailsUrl": "static/places/moscow_legends.json"
+            "title": place.title,
+            "placeId": place.id,
+            "detailsUrl": "static/places"
           }
-        },
-        {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [37.64, 55.753676]
-          },
-          "properties": {
-            "title": "Крыши24.рф",
-            "placeId": "roofs24",
-            "detailsUrl": "static/places/roofs24.json"
-          }
-        }
+        } for place in Place.objects.all()
       ]
     }
     return render(request, 'index.html', context={"places": places})
