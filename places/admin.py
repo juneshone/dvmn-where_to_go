4cda3b2442a8bc445480e32a677ec145839ae2f4
juneshone <shamrova.julia@mail.ru>
2024-06-11 21:06:55 +0700

    Add inline in admin interface

--- places/admin.py
+++ places/admin.py
@@ -3,10 +3,15 @@ from django.contrib import admin
 from places.models import Place, Imagery
 
 
+class ImageryInline(admin.TabularInline):
+    model = Imagery
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     search_fields = ('title',)
     list_display = ('title', 'description_short', 'lng', 'lat',)
+    inlines = [ImageryInline]
 
 
 @admin.register(Imagery)
