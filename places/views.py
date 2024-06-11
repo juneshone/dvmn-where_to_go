6cb18a713ecd16a53a9ed3e4b7d2c97673ca0f21
juneshone <shamrova.julia@mail.ru>
2024-06-11 20:25:23 +0700

    Download place detail from db

--- places/views.py
+++ places/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from places.models import Place
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
 
 def index(request):
@@ -17,7 +18,7 @@ def index(request):
           "properties": {
             "title": place.title,
             "placeId": place.id,
-            "detailsUrl": "static/places"
+            "detailsUrl": reverse("places", args=(place.id,))
           }
         } for place in Place.objects.all()
       ]
