846f9d637acb895304d5bc71d34dbc4f7e2af57b
juneshone <shamrova.julia@mail.ru>
2024-06-11 16:57:53 +0700

    Create API Endpoint

--- places/views.py
+++ places/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from places.models import Place
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+def index(request):
+    places = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [place.lng, place.lat]
+          },
+          "properties": {
+            "title": place.title,
+            "placeId": place.id,
+            "detailsUrl": "static/places"
+          }
+        } for place in Place.objects.all()
+      ]
+    }
+    return render(request, 'index.html', context={"places": places})
+
+
+def get_place(request, id):
+    place = get_object_or_404(Place, id=id)
+    return HttpResponse(place)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from where_to_go import views
+from places import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.show_start_page),
+    path('places/<int:id>/', views.get_place, name='places'),
+    path('', views.index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
