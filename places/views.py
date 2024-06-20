fd84201b0a5db2df4adcf36848c700221dfed6b3
juneshone <shamrova.julia@mail.ru>
2024-06-20 21:23:14 +0700

    Set same kind of quotes

--- places/views.py
+++ places/views.py
@@ -7,35 +7,35 @@ from django.urls import reverse
 
 def index(request):
     places = {
-      "type": "FeatureCollection",
-      "features": [
+      'type': 'FeatureCollection',
+      'features': [
         {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [place.lng, place.lat]
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [place.lng, place.lat]
           },
-          "properties": {
-            "title": place.title,
-            "placeId": place.id,
-            "detailsUrl": reverse("places", args=(place.id,))
+          'properties': {
+            'title': place.title,
+            'placeId': place.id,
+            'detailsUrl': reverse('places', args=(place.id,))
           }
         } for place in Place.objects.all()
       ]
     }
-    return render(request, 'index.html', context={"places": places})
+    return render(request, 'index.html', context={'places': places})
 
 
 def upload_place_detail(request, id):
     place = get_object_or_404(Place, id=id)
     response = {
-        "title": place.title,
-        "imgs": [imagery.image.url for imagery in place.imageries.all()],
-        "description_short": place.short_description,
-        "description_long": place.long_description,
-        "coordinates": {
-            "lat": place.lat,
-            "lng": place.lng
+        'title': place.title,
+        'imgs': [imagery.image.url for imagery in place.imageries.all()],
+        'description_short': place.short_description,
+        'description_long': place.long_description,
+        'coordinates': {
+            'lat': place.lat,
+            'lng': place.lng
         }
     }
     return JsonResponse(
